a833bd105909f07bf63593ea09a695174b2adb64
Poul Henriksen <pohe@zealand.dk>
2026-03-16 16:23:52 +0100

    Add project files.

---
+++ HotelDB26ConsoleStart/Models/Hotel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelDB26ConsoleStart.Models
+{
+    public class Hotel
+    {
+        public int HotelNr { get; set; }
+        public String Navn { get; set; }
+        public String Adresse { get; set; }
+
+        public Hotel()
+        {
+        }
+
+        public Hotel(int hotelNr, string navn, string adresse)
+        {
+            HotelNr = hotelNr;
+            Navn = navn;
+            Adresse = adresse;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(HotelNr)}: {HotelNr}, {nameof(Navn)}: {Navn}, {nameof(Adresse)}: {Adresse}";
+        }
+    }
+}
