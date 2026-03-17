ce4d1fa79d8435b8412d69321550974f0e44c7c2
Poul Henriksen <pohe@zealand.dk>
2026-03-17 07:50:35 +0100

    razor

---
+++ HotelLibrary/Services/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary.Services
+{
+    public abstract class Connection
+    {
+        protected String connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HotelDbtest2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    }
+}
---
+++ RazorHotelDB26/Pages/Hotels/GetAllHotels.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorHotelDB26.Pages.Hotels
+{
+    public class GetAllHotelsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
